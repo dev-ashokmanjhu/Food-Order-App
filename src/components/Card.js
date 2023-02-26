14ebadf94ce0d8fce6edd715d572d0892b3ca34c
dev-ashokmanjhu <dev.ashokmanjhu@gmail.com>
2023-02-26 13:04:53 +0530

    ui update

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,14 +4,14 @@ import { IMG_CDN_URL } from "../constants";
 const Card = ({ restaurant }) => {
   const { name, cuisines, area, cloudinaryImageId } = restaurant.data;
   return (
-    <div className="cardContainer">
+    <div className="border-2 rounded-md shadow-md text-center">
       <img
         className="cardimg"
         src={IMG_CDN_URL + cloudinaryImageId}
         alt="thumbnail"
       />
       <h2>{name}</h2>
-      <p>{cuisines.join(",")}</p>
+      <p className="text-xs break-words">{cuisines.join(",")}</p>
       <h4>{area}</h4>
     </div>
   );
